a374b61de4b2dc350d02af7e49c047fb644f454a
juri <ojl98@naver.com>
2024-03-07 00:02:26 +0900

    BoardDAO - getConnection(), pstmt로 sql문 생성

--- src/main/java/board/BoardDAO.java
+++ src/main/java/board/BoardDAO.java
@@ -2,6 +2,7 @@ package board;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import common.JdbcUtil;
 
@@ -21,7 +22,26 @@ public class BoardDAO {
 		String query = "insert into board(num, title, writer, content, regdate, cnt) "
 				+ "values(board_seq.nextval, ?, ?, ?, sysdate, 0)";
 		
+		//전체 삽입된 행 수
 		int ret = -1;
+		
+		try {
+			//커넥션풀로부터 Connection 객체를 받아옴
+			con = ju.getConnection();
+			//pstmt에 query 등록
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getWriter());
+			pstmt.setString(3, vo.getContent());
+			
+			//전체 삽입 된 행 수 리턴
+			ret = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(con, pstmt);
+		}//end of finally
 		
 		return ret;
 	}
