49d147b62565cdc03d141aa2ff72551ece95bf9b
juri <ojl98@naver.com>
2024-03-06 23:58:08 +0900

    BoardDAO - Connection, pstmt, query 준비

--- src/main/java/board/BoardDAO.java
+++ src/main/java/board/BoardDAO.java
@@ -1,5 +1,8 @@
 package board;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import common.JdbcUtil;
 
 public class BoardDAO {
@@ -9,5 +12,17 @@ public class BoardDAO {
 	//DI(의존주입)
 	public BoardDAO() {
 		ju = JdbcUtil.getInstance();
+	}
+	
+	//삽입(C) - 게시글 작성
+	public int insert (BoardVO vo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String query = "insert into board(num, title, writer, content, regdate, cnt) "
+				+ "values(board_seq.nextval, ?, ?, ?, sysdate, 0)";
+		
+		int ret = -1;
+		
+		return ret;
 	}
 }
