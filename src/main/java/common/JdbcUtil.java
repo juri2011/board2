04972091bfe8b1816b4dbb756d6bc79ed0adf0ee
juri <ojl98@naver.com>
2024-03-06 23:49:59 +0900

    JdbcUtil - Connection Pool 생성

--- src/main/java/common/JdbcUtil.java
+++ src/main/java/common/JdbcUtil.java
@@ -1,5 +1,8 @@
 package common;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class JdbcUtil {
@@ -12,6 +15,7 @@ public class JdbcUtil {
 	
 	/*
 		DataSource?
+		DriverManager의 대용으로 쓸 수 있다.
 		커넥션풀의 Connection 객체를 관리하기 위해 사용하는 객체.
 		필요한 Connection을 빌려주고 반납하는 작업을 한다.
 			1. lookup() 메소드로 DataSource 객체를 얻는다.
@@ -32,11 +36,22 @@ public class JdbcUtil {
 			/*
  				Class.forName()?
  				런타임 동적 로딩 : 실행 중에 JVM이 코드를 만나는 순간 클래스를 로딩
+ 				Class.forName()을 사용하면 필요 없는 driver를 모두 로드 할 필요가 없다.
  				밑의 코드는 Oracle Driver를 로드한다.
 			*/
 			Class.forName("oracle.jdbc.OracleDriver");
 			System.out.println("드라이버 로딩 성공");
+			
+			//DataSource 객체를 얻어 커넥션 풀을 생성하는 코드
+			//context.xml에 connection pool 설정 정보를 삽입해야 한다.
+			InitialContext ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:comp/env");
+			ds = (DataSource) envContext.lookup("jdbc/oracle");
+			System.out.println("Connection Pool 생성!");
+			
 		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NamingException e) {
 			e.printStackTrace();
 		}
 	}
@@ -45,7 +60,6 @@ public class JdbcUtil {
 	public static JdbcUtil getInstance() {
 		return instance;
 	}
-	
 	
 	
 }
