992e11e691c418cf3507d4d6abb4db510aae8735
juri <ojl98@naver.com>
2024-03-06 23:01:59 +0900

    JdbcUtil 클래스 싱글톤 처리

--- src/main/java/common/JdbcUtil.java
+++ src/main/java/common/JdbcUtil.java
@@ -1,5 +1,11 @@
 package common;
 
 public class JdbcUtil {
-	
+	/*
+		싱글톤 처리
+		싱글톤 패턴은 정보를 보관하고 공유하고자 하는 클래스를
+		한 번만 메모리에 할당하고 객체로 관리하기 위해 사용한다.
+	*/
+	private static JdbcUtil instance = new JdbcUtil();
+	private JdbcUtil() {}
 }
